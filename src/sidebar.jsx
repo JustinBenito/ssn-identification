7cbf49988ba7203ac77bfbff774720f3634d7d54
JustinBenito <justinbenito1974@gmail.com>
2024-05-26 02:33:51 +0530

    toaster push

--- src/sidebar.jsx
+++ src/sidebar.jsx
@@ -352,11 +352,12 @@ if(data && pitch && sounds){
           class=" hidden md:block  w-48 h-full mb-8 "
           aria-label="Sidebar"
         >
-          <div class="h-full px-3 py-4  overflow-y-auto bg-gray-50 dark:bg-gray-800">
+          <div class="h-full px-3 py-4  overflow-y-auto bg-gray-50 dark:bg-gray-800" >
             <ul class="space-y-8 font-medium ">
               <li>
                 <a
                   href="#"
+                  onClick={()=>{toast.success("Working on it");}}
                   class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                 >
                   <svg
@@ -369,13 +370,14 @@ if(data && pitch && sounds){
                     <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
                     <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
                   </svg>
-                  <span class="ms-3">Articulatory</span>
+                  <span  class="ms-3">Articulatory</span>
                 </a>
               </li>
 
               <li>
                 <a
                   href="#"
+                  onClick={()=>{toast.success("Working on it");}}
                   class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                 >
                   <svg
@@ -395,6 +397,7 @@ if(data && pitch && sounds){
               <li>
                 <a
                   href="#"
+                  onClick={()=>{toast.success("Working on it");}}
                   class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                 >
                   <svg
@@ -413,6 +416,7 @@ if(data && pitch && sounds){
 
               <li>
                 <a
+                onClick={()=>{toast.success("Working on it");}}
                   href="#"
                   class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                 >
